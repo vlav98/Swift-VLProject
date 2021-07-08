31e132437b3137d8c79e663a0152376c1a254caa
Valentine <valentinelav98@gmail.com>
2021-07-08 13:35:48 +0200

    Initial Commit

---
+++ ValentineLav/ValentineLavApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValentineLavApp.swift
+//  ValentineLav
+//
+//  Created by jpo on 08/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ValentineLavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
